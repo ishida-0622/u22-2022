50de2682591e89fa07dcfdf8a70d16c276a27a69
ishida-0622 <ken.babuo@gmail.com>
2022-06-13 13:42:24 +0900

    不要なコメントの削除

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const glob = require("glob");
 const entries = glob.sync("./src/**/*.js");
 
 module.exports = {
-    // エントリーポイントの設定
-    // entry: "./public/src/index.js",
     entry: entries,
     output: {
         path: path.resolve(__dirname, './public/dist'),
