4f940385dac341b58f462c5dbac00552ebbe418e
kentaro ishida <ken.babuo@gmail.com>
2022-06-18 00:25:15 +0900

    ユーザーのデータを取得する関数の追加

---
+++ src/auth/userData.js
@@ -0,0 +1,14 @@
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebaseConfig";
+
+const getUserData = () => {
+    const auth = getAuth(app);
+    const user = auth.currentUser;
+    console.log(user);
+    if (user) {
+        return user;
+    }
+    return null;
+}
+
+export default getUserData;
