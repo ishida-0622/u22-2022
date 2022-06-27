941a9f6b8c25454e219afe5b9cf41615c9ca490e
kentaro ishida <ken.babuo@gmail.com>
2022-06-27 14:45:46 +0900

    不要部分の削除

--- src/index.js
+++ src/index.js
@@ -6,34 +6,6 @@ import { auth, db } from "./firebase/firebaseConfig"
 import { signOut } from "firebase/auth"
 import Vue from "./vue"
 
-const vm = new Vue({
-    el: "#main",
-    data: {
-        test: "",
-        array: [],
-    },
-    methods: {
-        changeFunc: () => {
-            const a = {
-                name: "石田健太郎"
-            }
-            const b = {
-                name: "加藤木優斗"
-            }
-            const c = {
-                name: "石川"
-            }
-            const d = {
-                name: "加藤"
-            }
-            const arr = [a,b,c,d];
-            const e = vm.test;
-            const reg = new RegExp("^" + e);
-            vm.array = arr.filter(obj => obj.name.match(reg)).map(val => val.name);
-        }
-    },
-})
-
 $(function () {
     $("#test").on("click", async () => {
         // signUp("test001@example.com", "pass00");
