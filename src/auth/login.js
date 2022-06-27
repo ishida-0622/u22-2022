1881ba7f314f0ae880a4316957436a505fcac348
kentaro ishida <ken.babuo@gmail.com>
2022-06-27 19:44:27 +0900

    フォーマッターによる変更

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,17 +1,22 @@
 import { auth } from "../firebase/firebaseConfig";
-import { signInWithEmailAndPassword, AuthErrorCodes } from "firebase/auth"
+import { signInWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
 const login = (mail, pass) => {
-    signInWithEmailAndPassword(auth, mail, pass).then(() => {
-        // window.location.href = '/test.html';
-    }).catch((e) => {
-        const errorCode = e.code;
-        if (errorCode === AuthErrorCodes.USER_DELETED || errorCode === AuthErrorCodes.INVALID_PASSWORD) {
-            alert("メールアドレスもしくはパスワードが間違っています");
-        } else {
-            alert(errorCode);
-        }
-    });
-}
+    signInWithEmailAndPassword(auth, mail, pass)
+        .then(() => {
+            // window.location.href = '/test.html';
+        })
+        .catch((e) => {
+            const errorCode = e.code;
+            if (
+                errorCode === AuthErrorCodes.USER_DELETED ||
+                errorCode === AuthErrorCodes.INVALID_PASSWORD
+            ) {
+                alert("メールアドレスもしくはパスワードが間違っています");
+            } else {
+                alert(errorCode);
+            }
+        });
+};
 
 export default login;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import signUp from "./auth/signup";
-import $ from "jquery"
-import getUserData from "./auth/userData"
-import { doc, setDoc, addDoc, getDoc, collection, getDocs, query, where } from "firebase/firestore"
-import { auth, db } from "./firebase/firebaseConfig"
-import { signOut } from "firebase/auth"
-import Vue from "./vue"
+import $ from "jquery";
+import getUserData from "./auth/userData";
+import {
+    doc,
+    setDoc,
+    addDoc,
+    getDoc,
+    collection,
+    getDocs,
+    query,
+    where,
+} from "firebase/firestore";
+import { auth, db } from "./firebase/firebaseConfig";
+import { signOut } from "firebase/auth";
+import Vue from "./vue";
 
 $(function () {
     $("#test").on("click", async () => {
