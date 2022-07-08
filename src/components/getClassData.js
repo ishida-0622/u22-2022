27764dfe50aa12f122ec41f62e1b90146ea96e66
kentaro ishida <ken.babuo@gmail.com>
2022-07-08 19:36:47 +0900

    コメントの修正

--- src/components/getClassData.js
+++ src/components/getClassData.js
@@ -4,7 +4,7 @@ import { getDoc, doc } from "firebase/firestore";
 /**
  * クラス名に対応するクラスの情報を返す
  * @param {string} className クラス名
- * @returns クラス名に対応するクラスの情報を
+ * @returns クラス名に対応するクラスの情報
  */
 const getClassData = async (className) => {
     // firestoreのclassコレクションからclassNameが一致するドキュメントを取得して変数に代入
