a51b351e1d2893e9d39de3c6d2e481394a92d5e2
kentaro ishida <ken.babuo@gmail.com>
2022-07-08 19:29:20 +0900

    存在確認の追加 コメントの追加とフォーマッターによる修正

--- src/components/getClassData.js
+++ src/components/getClassData.js
@@ -1,6 +1,11 @@
 import { db } from "../firebase/firebaseConfig";
 import { getDoc, doc } from "firebase/firestore";
 
+/**
+ * クラス名に対応するクラスの情報を返す
+ * @param {string} className クラス名
+ * @returns クラス名に対応するクラスの情報を
+ */
 const getClassData = async (className) => {
     // firestoreのclassコレクションからclassNameが一致するドキュメントを取得して変数に代入
     const firestoreDocument = await getDoc(doc(db, `class/${className}`));
@@ -8,8 +13,13 @@ const getClassData = async (className) => {
     // データを変数に代入
     const firestoreData = firestoreDocument.data();
 
+    // 存在していなかったらnullを返す
+    if (firestoreData === undefined) {
+        return null;
+    }
+
     // 変数に代入したデータを返す
-    return firestoreData
-}
+    return firestoreData;
+};
 
 export default getClassData;
