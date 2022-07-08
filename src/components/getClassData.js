4360fc3d5051237954d2c75b0a1352fd245210d5
yuma sasaki <maruo.genshin1@gmail.com>
2022-07-08 14:03:19 +0900

    getClassDataです。確認よろしくお願いします。

---
+++ src/components/getClassData.js
@@ -0,0 +1,15 @@
+import { db } from "../firebase/firebaseConfig";
+import { getDoc, doc } from "firebase/firestore";
+
+const getClassData = async (className) => {
+    // firestoreのclassコレクションからclassNameが一致するドキュメントを取得して変数に代入
+    const firestoreDocument = await getDoc(doc(db, `class/${className}`));
+
+    // データを変数に代入
+    const firestoreData = firestoreDocument.data();
+
+    // 変数に代入したデータを返す
+    return firestoreData
+}
+
+export default getClassData;
