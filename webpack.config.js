c1b5b6c7edc6a9f60a02d6c7f73f6b58a77c1d2e
kentaro ishida <ken.babuo@gmail.com>
2022-07-14 15:14:57 +0900

    ビルドするファイルの追加

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,15 @@ const glob = require("glob");
 const entries = glob.sync("./src/**/*.js");
 
 module.exports = {
-    entry: entries,
+    entry: {
+        all: entries,
+        stuScoreInput: "./src/student/score_input.js",
+        stuScoreCfm: "./scr/student/score_cfm.js",
+        teacherScoreInput: "./scr/teacher/score_input.js",
+        teacherScoreCfm: "./scr/teacher/score_cfm.js",
+    },
     output: {
         path: path.resolve(__dirname, './public/dist'),
-        filename: 'index.js'
+        filename: "[name].bundle.js"
     }
 };
