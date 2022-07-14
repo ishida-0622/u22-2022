66dc4d3f1cbea18016514b9b528362ca1eca07d9
Kanato Sakuma <tky2102051@stu.o-hara.ac.jp>
2022-07-14 16:24:56 +0900

    ていく３

--- src/components/getClassList.js
+++ src/components/getClassList.js
@@ -2,7 +2,7 @@ import getStuPerfData from "./getStuPerfData";
 import getTeacherData from "./getTeacherData";
 import getStuData from "./getStuData";
 import { collection, getDocs } from "firebase/firestore";
-import { teacherClassDataConverter, teacherDataConverter } from "../firebase/firestoreTypes";
+import { teacherClassDataConverter } from "../firebase/firestoreTypes";
 
 /**
  * useridに対応するクラスの一覧を返す
@@ -10,12 +10,11 @@ import { teacherClassDataConverter, teacherDataConverter } from "../firebase/fir
  * @returns useridに対応するクラスの一覧
  */
 const getClassList = async (uid) => {
-
     // uidが講師かどうか
-    const isTeacher = await getTeacherData(uid) !== null;
+    const isTeacher = (await getTeacherData(uid)) !== null;
 
     // uidが生徒化どうか
-    const isStudent = await getStuData(uid) !== null;
+    const isStudent = (await getStuData(uid)) !== null;
     // 生徒の場合
     if (isStudent) {
         // コレクションを変数に代入
@@ -29,7 +28,11 @@ const getClassList = async (uid) => {
 
     // 講師の場合
     if (isTeacher) {
-        const firestoreCollection= await getDocs(collection(db,`users/${uid}/class`).withConverter(teacherDataConverter));
+        const firestoreCollection = await getDocs(
+            collection(db, `users/${uid}/class`).withConverter(
+                teacherClassDataConverter
+            )
+        );
 
         // クラス名のリストを変数に代入
         const classList = firestoreCollection.map((value) => value.class_name);
