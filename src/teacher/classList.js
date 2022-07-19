d159ec43dfeab7d4d591b75b72f1e6ca610afb42
Kanato Sakuma <tky2102051@stu.o-hara.ac.jp>
2022-07-19 16:28:36 +0900

    クラス一覧画面のhtmlとjsが完成しました

--- src/teacher/classList.js
+++ src/teacher/classList.js
@@ -1,55 +1,82 @@
 import getClassList from "../components/getClassList";
 import getClassData from "../components/getClassData";
 import getTeacherData from "../components/getTeacherData";
-// import getUserData from "../auth/userData";
+import getUserData from "../auth/userData";
 import $ from "jquery";
 import { db } from "../firebase/firebaseConfig";
-import { doc, deleteDoc, getDoc } from "firebase/firestore";
-// import getUserData from "../auth/userData";
+import { doc, deleteDoc } from "firebase/firestore";
 
-// const userData = await getUserData().uid;
-
-// const uid = userData.uid;
+/**
+ * クラス一覧画面のjs
+ */
 const main = async () => {
-    const uid = "zTp17yLgZOQqgIAQ1iAXFmnm2Lq2";
+    // 現在ログイン出来ているかどうかを確認　出来ていなければログイン画面に飛ばす
+    const userData = getUserData();
+    if (userData === null) {
+        window.location.href = "/login.html";
+        return;
+    }
 
+    // useridを変数に代入
+    const uid = userData.uid;
+
+    // classListを変数に代入
     const classList = await getClassList(uid);
 
+    //　HTML 要素を取得
     const element = document.getElementById("class-list");
 
+    // クラス一覧の表示
     classList.forEach(async (value) => {
+        // クラスデータを取得
         const classData = await getClassData(value);
+
+        // クラスを担当する講師名を取得
         const teacherDocument = await getTeacherData(classData.teachers[0]);
+
+        // 講師名を変数に代入
         const teacherName =
             teacherDocument.last_name + " " + teacherDocument.first_name;
 
         if (classData !== null) {
+            // tableの中身を表示
             element.insertAdjacentHTML(
                 "beforeend",
                 "<tr><td>" +
                     classData.class_name +
                     "</td><td>" +
                     teacherName +
+                    // クラス削除ボタンで表示非表示の切り替え
                     `</td><td class = "del-button"><button id = "${classData.class_name}" class="del-class-button">消去</button></td></tr>`
             );
+            $(".del-button").slideToggle(0);
         }
     });
 };
 
 /**
- *
+ * クラスを削除する
+ * 削除が実行出来たかどうかを bool型で返す
  * @param {string} className クラス名
  */
 const delClass = async (className) => {
+    // クラスデータを取得
     const document = await getClassData(className);
-    if (document === null) {
-        return;
-    }
-    document.teachers.forEach(async (v) => {
-        await deleteDoc(doc(db, `users/${v}/class/${className}`));
-    });
+
+    // return用の変数の初期設定
     let res = false;
-    // classのclassNameのドキュメントを削除
+
+    // クラスデータがnullならば faluse を返す
+    if (document === null) {
+        return res;
+    }
+
+    // teachers/class/class-name の一致する class-name を削除する
+    document.teachers.forEach(async (teacherName) => {
+        await deleteDoc(doc(db, `users/${teacherName}/class/${className}`));
+    });
+
+    // classのclassNameのドキュメントを削除する関数
     await deleteDoc(doc(db, `class/${className}`))
         .then(() => {
             res = true;
@@ -63,16 +90,17 @@ const delClass = async (className) => {
 // クリックをされた時、delClassを実行
 $(function () {
     $("#show-del-button").on("click", async () => {
-        $(".del-button").slideToggle("");
+        // 削除ボタンを表示する
+        $(".del-button").slideToggle(0);
     });
 
     $(document).on("click", ".del-class-button", async (event) => {
-        console.log("hello");
         // 「OK」時の処理開始 ＋ 確認ダイアログの表示
-        if (window.confirm("本当にいいんですね？")) {
+        if (window.confirm(event.target.id + "を削除しても良いですか？")) {
             await delClass(event.target.id);
+
             // リロード
-            location.href = "./";
+            location.reload();
         }
         // 「OK」時の処理終了
 
@@ -85,4 +113,5 @@ $(function () {
     });
 });
 
+// mainを実行
 main();
