e9ce8cfedfce5fe7085958596f80c3bcf45e0a86
kentaro ishida <ken.babuo@gmail.com>
2022-08-09 22:06:28 +0900

    フォーマッターによる修正

--- src/teacher/scoreInput.js
+++ src/teacher/scoreInput.js
@@ -2,22 +2,22 @@ import getUserData from "../auth/getUserData";
 import getTestData from "../components/getTestData";
 import getClassList from "../components/getClassList";
 import getTestList from "../components/getTestList";
-import getStuInClass from '../components/getStuInClass'
+import getStuInClass from "../components/getStuInClass";
 import { db } from "../firebase/firebaseConfig";
 import { doc, updateDoc } from "firebase/firestore";
 
 // 各HTML要素を取得する
 // テストセレクトボックスを含む要素を取得する
-const selectTestWrap = document.querySelector('#test-selectbox');
+const selectTestWrap = document.querySelector("#test-selectbox");
 
 // テストセレクトボックスを生成し、HTML要素を取得する
-const selectBox = document.createElement('select');
+const selectBox = document.createElement("select");
 
 // テーブルを生成し、HTML要素を取得する
 const tbl = document.querySelector("table");
 
 // 登録ボタンのHTML要素を取得する
-const btn = document.querySelector('#cfm-dialog');
+const btn = document.querySelector("#cfm-dialog");
 
 // ログイン中のユーザ（講師）のIDを取得する
 const uid = getUserData().uid;
@@ -29,21 +29,23 @@ const uclasses = getClassList(uid);
  * DBからテスト名を取得し、テストセレクトボックスに与える
  */
 const main = async () => {
-
     // クラスを渡し、テスト情報を取得する
-    let tests = (await Promise.all(uclasses.map(async (uclass) => await getTestList(uclass)))).flat();
+    let tests = (
+        await Promise.all(
+            uclasses.map(async (uclass) => await getTestList(uclass))
+        )
+    ).flat();
 
     // テストのデフォルト値を設定する
-    tests.unshift({ test_name: '未選択' });
+    tests.unshift({ test_name: "未選択" });
 
     // DBから取得したテスト名を走査する
     tests.forEach((test) => {
-
         // option要素を生成する
-        const option = document.createElement('option');
+        const option = document.createElement("option");
 
         // 画面表示用のテスト名を渡す
-        option.textContent = test.test_name;  // 画面表示用
+        option.textContent = test.test_name; // 画面表示用
 
         // 内部管理用のテスト名を取得する（画面表示用と同様）
         option.value = test.test_name;
@@ -54,15 +56,12 @@ const main = async () => {
 
     // テストセレクトボックスの要素selectをselectWrapに加える
     selectTestWrap.appendChild(selectBox);
-
-}
-
+};
 
 /**
  * DBからクラスの生徒名を取得し、テーブルを追加する
  */
 const addTable = async () => {
-
     // 選択されたテストを渡してテストデータを取得し、テストのクラス名を取得する
     const testClass = await getTestData(selectBox.value);
     if (testClass === null) {
@@ -78,11 +77,10 @@ const addTable = async () => {
     // 入力があったら、scoreCheckを実行する
     const scoreInput = (event) => {
         scoreCheck(event.target.value);
-    }
+    };
 
     // 生徒名を走査する
     stuDatas.forEach((stu) => {
-
         // テーブルに１行追加する
         const row = tbl.insertRow(-1);
 
@@ -90,7 +88,7 @@ const addTable = async () => {
         const stuCell = row.insertCell(-1);
 
         // 生徒名のセルに取得した生徒名を挿入する
-        stuCell.innerHTML = `<p>${stu.last_name}${stu.first_name}</p>`
+        stuCell.innerHTML = `<p>${stu.last_name}${stu.first_name}</p>`;
 
         // 点数入力欄のセルを生成する
         const scoreCell = row.insertCell(-1);
@@ -110,21 +108,21 @@ const addTable = async () => {
         // 設定した入力欄を点数入力欄にする
         scoreCell.appendChild(element);
     });
-}
+};
 
 /**
  * 点数入力欄に、点数の最低点未満または最高点より上の値が入力されたら、アラートで通知する関数
  * @param socre 入力された点数
  */
 const scoreCheck = (score) => {
-
     // 入力された点数が、テストの点数の範囲外であった場合
-    if (( Number(score) < tests.min_score ) || ( tests.max_score < Number(score) )) {
-
+    if (Number(score) < tests.min_score || tests.max_score < Number(score)) {
         // アラートで通知する
-        alert('範囲外の点数が入力されました。登録するには、テスト情報を編集して下さい。');
+        alert(
+            "範囲外の点数が入力されました。登録するには、テスト情報を編集して下さい。"
+        );
     }
-}
+};
 
 /**
  * テストの点数をDBに更新する関数
@@ -133,10 +131,9 @@ const scoreCheck = (score) => {
  * @param {number} score 点数
  */
 const scoreUpdate = async (uid, testName, score) => {
-
     // アップデートするテストの点数の情報
     const updateUserData = {
-        score: score
+        score: score,
     };
 
     // 該当する生徒のテストの点数が変更される
@@ -147,32 +144,28 @@ const scoreUpdate = async (uid, testName, score) => {
  * 登録の意思を確認するダイアログを表示する。
  */
 const cfm = () => {
-
     // テストセレクトボックスの値が未定義の場合
-    if (selectBox.value == '未選択') {
-
+    if (selectBox.value == "未選択") {
         // テストの選択がないことをアラートで通知する
-        alert('テストの選択がされていません');
+        alert("テストの選択がされていません");
 
         // テストの選択がある場合
     } else {
-
         // 登録の意思を確認するダイアログを表示する
-        const result = window.confirm('登録しますか？');
+        const result = window.confirm("登録しますか？");
 
         // OKを選択されたら、
         if (result) {
-
             // テーブル部分のHTML要素を取得する
             const element = document.getElementById("tbl");
 
             // 点数入力の有無をチェックするflgを定義する
-            let flg = true
+            let flg = true;
 
             // テーブルの生徒名、点数入力欄の値を走査する
             Array.from(element.rows).forEach(async (val, i) => {
-
-                if (i != 0) {  // カラム名を除く2行目以降を処理
+                if (i != 0) {
+                    // カラム名を除く2行目以降を処理
 
                     // 一行取得する
                     const cell = Array.from(val.cells);
@@ -184,13 +177,12 @@ const cfm = () => {
                     const score = cell[1].children[0].value;
 
                     // 点数の入力があった場合にflgをfalseにする
-                    if (String(score) != '') {
-                        flg = false
+                    if (String(score) != "") {
+                        flg = false;
                     }
 
                     // 点数の入力があった場合
-                    if (String(score) != '') {
-
+                    if (String(score) != "") {
                         // 生徒のidから生徒のuidを取得する
                         const uidStu = await getUid(id);
 
@@ -198,30 +190,27 @@ const cfm = () => {
                         await scoreUpdate(uidStu, selectBox.value, score);
                     }
                 }
-            })
+            });
 
             // 点数の入力が一つもなかった場合
             if (flg) {
-
                 // 点数の入力がないことをアラートで通知する
-                alert('点数の入力がありませんでした');
+                alert("点数の入力がありませんでした");
 
-            // 点数の入力があった場合
+                // 点数の入力があった場合
             } else {
-
                 // 正常完了をアラートで通知する
-                alert('登録が完了しました');
+                alert("登録が完了しました");
             }
-
-        }   // キャンセルを選択されたら、何もしない
+        } // キャンセルを選択されたら、何もしない
     }
-}
+};
 
 // ページがロードされたら、テストセレクトボックスを画面に表示する
-window.addEventListener('load', main);
+window.addEventListener("load", main);
 
 // テストセレクトボックスでテストが選択されたら、生徒名と点数入力欄テーブルをロードする
-selectTestWrap.addEventListener('change', addTable);
+selectTestWrap.addEventListener("change", addTable);
 
 // 登録するボタンをクリックされたら、登録の意思を確認するダイアログを表示する
-btn.addEventListener('click', cfm);
+btn.addEventListener("click", cfm);
--- src/teacher/testList.js
+++ src/teacher/testList.js
@@ -11,7 +11,6 @@ import { doc, deleteDoc } from "firebase/firestore";
  * テスト一覧画面のjs
  */
 const main = async () => {
-
     // ユーザ情報を取得する
     const userData = getUserData();
 
@@ -28,32 +27,33 @@ const main = async () => {
     const classList = await getClassList(uid);
 
     // classListを渡し、testList（テスト情報）を変数に代入する
-    const testList = (await Promise.all(classList.map(async (className) => await getTestList(className)))).flat();
+    const testList = (
+        await Promise.all(
+            classList.map(async (className) => await getTestList(className))
+        )
+    ).flat();
 
     // テスト一覧を表示するテーブル部分のHTML要素を取得する
     const element = document.getElementById("#test-list");
 
     // テスト情報を走査し、テスト一覧の表示する
     testList.forEach(async (test) => {
-
         // テスト情報がある場合
         if (test !== null) {
-
             // tableの中身を表示
             element.insertAdjacentHTML(
                 "beforeend",
                 "<tr><td>" +
-                // テスト名
-                test.test_name +
-                "</td><td>" +
-                // テストの最高得点
-                test.max_score +
-                "</td><td>" +
-                // テストの最高得点
-                test.min_score +
-
-                // テスト削除ボタンで表示非表示の切り替え
-                `</td><td class = "del-button"><button id = "${test.test_name}" class="del-test-button">消去</button></td></tr>`
+                    // テスト名
+                    test.test_name +
+                    "</td><td>" +
+                    // テストの最高得点
+                    test.max_score +
+                    "</td><td>" +
+                    // テストの最高得点
+                    test.min_score +
+                    // テスト削除ボタンで表示非表示の切り替え
+                    `</td><td class = "del-button"><button id = "${test.test_name}" class="del-test-button">消去</button></td></tr>`
             );
             $(".del-button").slideToggle(0);
         }
@@ -66,7 +66,6 @@ const main = async () => {
  * @param {string} testName テスト名
  */
 const delTest = async (testName) => {
-
     // 取得したテスト名に一致するテストの情報を取得し、testDataに代入する
     const testData = await getTestData(testName);
 
@@ -75,30 +74,28 @@ const delTest = async (testName) => {
 
     // classDataの生徒IDを走査し、user/${生徒のID}/tests/${テスト名} の一致するドキュメントを削除する
     classData.students.forEach(async (stuId) => {
-        await deleteDoc(doc(db, `users/${stuId}/class/tests/${testName}`))
+        await deleteDoc(doc(db, `users/${stuId}/class/tests/${testName}`));
     });
 
     // tests内のtestNameに一致するドキュメントを削除する関数
-    await deleteDoc(doc(db, `tests/${testName}`))
+    await deleteDoc(doc(db, `tests/${testName}`));
 };
 
 // クリックをされた時、delTestを実行
 $(function () {
     $("#show-del-button").on("click", async () => {
-
         // 削除ボタンを表示する
         $(".del-button").slideToggle(0);
     });
 
     $(document).on("click", ".del-test-button", async (event) => {
-
         // 「OK」時の処理開始 ＋ 確認ダイアログの表示
         if (window.confirm(event.target.id + "を削除しても良いですか？")) {
             await delTest(event.target.id);
             // リロード
             location.reload();
         }
-        
+
         // 「OK」時の処理終了
         // 「キャンセル」時の処理開始
         else {
