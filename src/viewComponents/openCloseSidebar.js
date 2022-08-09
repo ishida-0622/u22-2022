9de5b9e3aa98140a6c8a7951b28ea75bb49fc06a
kentaro ishida <ken.babuo@gmail.com>
2022-08-09 22:13:29 +0900

    サイドバー開閉のjs onclickイベントの追加

--- src/viewComponents/openCloseSidebar.js
+++ src/viewComponents/openCloseSidebar.js
@@ -1,9 +1,14 @@
+import $ from "jquery";
+
 // サイドバーを開閉する際の動きの実装
 
 const openMenu = () => {
-    $('.side_menu').css('animation-name', 'openMenu');
-}
+    $(".side_menu").css("animation-name", "openMenu");
+};
 
 const closeMenu = () => {
-    $('.side_menu').css('animation-name', 'closeMenu');
-}
+    $(".side_menu").css("animation-name", "closeMenu");
+};
+
+$("#open").on("click", openMenu);
+$("#close").on("click", closeMenu);
