29d6f5725cb399c50b695ce8fc4b5bdfe6df7086
Masato Katogi <science-physics1308.tm@ezweb.ne.jp>
2022-08-09 18:47:56 +0900

    base.htmlのscriptをviewComponentsにファイル分割、及びhtmlの編集

---
+++ src/viewComponents/openCloseSidebar.js
@@ -0,0 +1,9 @@
+// サイドバーを開閉する際の動きの実装
+
+const openMenu = () => {
+    $('.side_menu').css('animation-name', 'openMenu');
+}
+
+const closeMenu = () => {
+    $('.side_menu').css('animation-name', 'closeMenu');
+}
