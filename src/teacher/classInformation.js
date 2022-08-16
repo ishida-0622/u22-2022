b44a5b55495c1885bbcb4d5994d2175fc28f190b
kent satomichi <tky2102096@stu.o-hara.ac.jp>
2022-08-16 18:32:43 +0900

    wip

--- src/teacher/classInformation.js
+++ src/teacher/classInformation.js
@@ -1,14 +1,23 @@
 import getClassData from "../components/getClassData";
 import getTeacherData from "../components/getTeacherData";
+import getStuData from "../components/getStuData";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "firebase/firebaseConfig";
 
 const main = async () => {
     //URL取得
     const url = new URL(window.location.href);
     const param = url.searchParams;
     const className = param.get('class_name');
+
     const classInformtion = await getClassData(className);
     const teacherInformtion = await getTeacherData(classInformtion.teachers[0]);
     const teacherName = teacherInformtion.last_name + teacherInformtion.first_name;
+
+    classInformtion.students.forEach(async (uid) => {
+        const studentData = await getStuData(uid);
+        const studentRate = (await getDoc(doc(db, `users/${uid}/class/${className}`))).data().rate;
+    })
 
     document.getElementById("class_name").innerHTML = className;
     document.getElementById("teacher_name").innerHTML = teacherName;
