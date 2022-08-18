b029011b32f18c4099d50c704a7eaf1139b65f84
kentaro ishida <ken.babuo@gmail.com>
2022-08-18 19:07:13 +0900

    ログインしているユーザーの名前とIDをサイドメニューに反映させる処理の追加

--- src/init.js
+++ src/init.js
@@ -1,4 +1,7 @@
+import { doc, getDoc } from "firebase/firestore";
 import getUserData from "./auth/getUserData";
+import { db } from "./firebase/firebaseConfig";
+import { userDataConverter } from "./firebase/firestoreTypes";
 
 const main = async () => {
     const user = await getUserData();
@@ -6,6 +9,21 @@ const main = async () => {
     if (user) {
         if (url === "/login.html" || url === "/signup.html") {
             location.href = "/";
+        }
+
+        const userData = (
+            await getDoc(
+                doc(db, `users/${user.uid}`).withConverter(userDataConverter)
+            )
+        ).data();
+
+        const idElement = document.getElementById("user-id");
+        if (idElement) {
+            idElement.textContent = userData.id;
+        }
+        const nameElement = document.getElementById("user-name");
+        if (nameElement) {
+            nameElement.textContent = userData.last_name + userData.first_name;
         }
     } else {
         if (url !== "/login.html" && url !== "/signup.html") {
