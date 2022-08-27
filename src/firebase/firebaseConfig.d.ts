d6a7bc94cb35b33e77f3acec53521e67e188249c
kentaro ishida <ken.babuo@gmail.com>
2022-08-27 14:39:10 +0900

    storageの追加

--- src/firebase/firebaseConfig.d.ts
+++ src/firebase/firebaseConfig.d.ts
@@ -1,6 +1,8 @@
 import { FirebaseApp } from "firebase/app";
 import { Auth } from "firebase/auth";
 import { Firestore } from "firebase/firestore";
+import { FirebaseStorage } from "firebase/storage";
 export const app: FirebaseApp;
 export const auth: Auth;
 export const db: Firestore;
+export const storage: FirebaseStorage;
