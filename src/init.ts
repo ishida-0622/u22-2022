ae0e2191d912dba241abd3ccc800e197a141b319
kentaro ishida <ken.babuo@gmail.com>
2022-08-27 14:36:38 +0900

    アイコン読み込みの追加

--- src/init.ts
+++ src/init.ts
@@ -2,6 +2,7 @@ import { doc, getDoc } from "firebase/firestore";
 import getUserData from "./auth/getUserData";
 import { db } from "./firebase/firebaseConfig";
 import { userDataConverter } from "./firebase/firestoreTypes";
+import fileDownload from "./storage/fileDownload";
 
 const main = async () => {
     const user = await getUserData();
@@ -12,7 +13,7 @@ const main = async () => {
             await getDoc(
                 doc(db, `users/${user.uid}`).withConverter(userDataConverter)
             )
-        ).data();
+        ).data()!;
 
         if (userData.type === "student") {
             if (
@@ -51,6 +52,13 @@ const main = async () => {
         if (nameElement) {
             nameElement.textContent = userData.last_name + userData.first_name;
         }
+        const icon = document.getElementById("icon") as HTMLImageElement | null;
+        if (icon) {
+            const url = await fileDownload(user.uid);
+            if (url) {
+                icon.src = url;
+            }
+        }
     } else {
         if (path !== "/login.html" && path !== "/signup.html") {
             location.href = "/login.html";
@@ -65,5 +73,5 @@ const toInfo = () => {
 };
 
 if (document.getElementById("icon")) {
-    document.getElementById("icon").onclick = toInfo;
+    document.getElementById("icon")!.onclick = toInfo;
 }
