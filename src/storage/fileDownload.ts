2e252dc14130575459fbbc82776967769101aba5
kentaro ishida <ken.babuo@gmail.com>
2022-08-28 16:22:24 +0900

    rejectをresolveに変更

--- src/storage/fileDownload.ts
+++ src/storage/fileDownload.ts
@@ -2,14 +2,14 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { storage } from "../firebase/firebaseConfig";
 
 const fileDownload = async (path: string): Promise<string | null> => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const pathRef = ref(storage, path);
         getDownloadURL(pathRef)
             .then((url) => {
                 resolve(url);
             })
             .catch(() => {
-                reject(null);
+                resolve(null);
             });
     });
 };
